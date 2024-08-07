ddb3132f44d474520fb10b145e8e7a1ad64f2ef7
ThIsJaCk <y23657689@gmail.com>
2024-08-08 04:55:29 +0800

    首頁

---
+++ src/Main.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<div class="w-full h-full flex flex-row justify-center items-center bg-blue-500">
+    <span class="text-6xl text-bold text-zinc-50">Hello World</span>
+</div>
+</template>
+
+<style scoped>
+</style>
