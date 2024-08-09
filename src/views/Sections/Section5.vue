fd673130fc3e53b0d86845753bd7af3491075ec5
ThIsJaCk <y23657689@gmail.com>
2024-08-09 08:31:22 +0800

    Page5 響應式

--- src/views/Sections/Section5.vue
+++ src/views/Sections/Section5.vue
@@ -9,21 +9,21 @@ const image3 = new URL('@/assets/images/section5/3.png', import.meta.url).href;
 </script>
 
 <template>
-<section class="w-full h-min-screen flex flex-col justify-center">
-    <div class="w-full flex flex-col justify-center items-center" style="height: 900px;">
-        <div class="w-full h-2/5 mb-2">
+<section class="w-full flex flex-col justify-center">
+    <div class="w-full flex flex-col justify-center items-center">
+        <div class="w-full h-24 sm:h-48 md:h-96 lg:h-[36rem] mb-2">
             <ImageContainer :src="image1" alt="" />
         </div>
-        <div class="w-full h-3/5 flex flex-row justify-center items-center">
-            <div class="w-1/2 h-full mr-2">
+        <div class="w-full h-28 sm:h-48 md:h-96 lg:h-[36rem] flex flex-row justify-center items-center">
+            <div class="w-full w-1/2 h-full mr-2">
                 <ImageContainer :src="image2" alt="" />
             </div>
-            <div class="w-1/2 h-full">
+            <div class="w-full w-1/2 h-full">
                 <ImageContainer :src="image3" alt="" />
             </div>
         </div>
     </div>
-    <div class="w-full flex justify-center items-center" style="height: 360px;">
+    <div class="w-full flex justify-center items-center h-24 lg:h-48">
     </div>
 </section>
 </template>
