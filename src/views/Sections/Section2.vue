737f9b13e52ec81a679fe3e547c8b23a0d9b4670
ThIsJaCk <y23657689@gmail.com>
2024-08-09 08:17:36 +0800

    Parallex Scrolling

--- src/views/Sections/Section2.vue
+++ src/views/Sections/Section2.vue
@@ -6,10 +6,16 @@ const bgUrl = new URL('@/assets/images/section2/map.png', import.meta.url).href;
 </script>
 
 <template>
-<section class="w-full h-screen flex justify-center items-center">
-    <Transition name="fade" appear>
+<section class="w-full h-screen flex justify-center items-center relative">
+    <div class="w-full h-full flex justify-center items-center bg-fixed bg-cover" :style="{ backgroundImage: `url(${bgUrl})` }">
+    </div>
+    <div class="absolute w-full h-full bg-black bg-opacity-20 z-10 hover:bg-opacity-5 transition-1000-out">
+        
+    </div>
+
+    <!-- <Transition name="fade" appear>
         <ImageContainer :src="bgUrl" alt="" />
-    </Transition>
+    </Transition> -->
 </section>
 </template>
 
