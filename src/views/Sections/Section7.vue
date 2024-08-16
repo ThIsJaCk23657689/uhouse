952ada882bbd2eb6ca290bd237bc1d3ae8b25b01
ThIsJaCk <y23657689@gmail.com>
2024-08-16 10:31:56 +0800

    Finish Page 7

--- src/views/Sections/Section7.vue
+++ src/views/Sections/Section7.vue
@@ -1,11 +1,64 @@
 <script setup lang="ts">
 import { ref, watch, onMounted } from 'vue';
+import ImageContainer from '@/components/ImageContainer.vue';
+
+const image1 = new URL('@/assets/images/section7/1.png', import.meta.url).href;
+const image2 = new URL('@/assets/images/section7/2.png', import.meta.url).href;
+const image3 = new URL('@/assets/images/section7/3.png', import.meta.url).href;
+const image4 = new URL('@/assets/images/section7/4.png', import.meta.url).href;
 
 </script>
 
 <template>
-<section class="w-full flex justify-center items-center bg-yellow-50">
-    <img src="@/assets/images/section7/7.png" alt="" class="object-cover">
+<section class="w-full flex flex-col justify-center items-center pt-24">
+
+    <div class="w-full flex flex-row justify-center items-start mb-64">
+
+        <!-- 左側 -->
+        <div class="w-1/2 h-full flex flex-col justify-start items-end pr-8">
+
+            <div class="w-3/5 h-[28rem] mb-2">
+                <ImageContainer :src="image1" alt="" />
+            </div>
+
+            <div class="w-3/5 h-[28rem] mb-32">
+                <ImageContainer :src="image2" alt="" />
+            </div>
+
+            <!-- 文字 -->
+            <div class="w-3/5 mb-12">
+                <p class="text-primary-300 font-bold text-[0.5rem] sm:text-base xl:text-xl">
+                    精品就是生活，生活就是精品<br>
+                    以高級訂製概念，打造居家質感<br>
+                    國際級頂規選品，有機能也要夠美型<br>
+                    夢想清單　美好一室，U ARE UNIQUE！<br>
+                </p>
+            </div>
+
+        </div>
+
+        <!-- 右側 -->
+        <div class="w-1/2 h-full flex flex-col justify-start items-start pt-32 pl-8">
+
+            <div class="w-3/5 h-[62rem] mb-2">
+                <ImageContainer :src="image3" alt="" />
+            </div>
+
+            <div class="w-3/5 h-[24rem]">
+                <ImageContainer :src="image4" alt="" />
+            </div>
+    
+        </div>
+
+
+
+    </div>
+
+
+    
+
+
+
 </section>
 </template>
 
